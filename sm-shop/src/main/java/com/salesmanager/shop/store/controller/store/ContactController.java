513450c7d346d365a9ace32fe95dc81fd6da008e
rajibroy-wipro <54103367+rajibroy-wipro@users.noreply.github.com>
2021-07-17 19:40:41 +0530

    Update ContactController.java

--- sm-shop/src/main/java/com/salesmanager/shop/store/controller/store/ContactController.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/controller/store/ContactController.java
@@ -66,6 +66,7 @@ public class ContactController extends AbstractController {
 	public String display(Model model, HttpServletRequest request, HttpServletResponse response, Locale locale) throws Exception {
 		System.out.println("Changes in ContactController");
 		System.out.println("Changes in ContactController");
+		System.out.println("Changes in ContactController");
 
 		MerchantStore store = (MerchantStore)request.getAttribute(Constants.MERCHANT_STORE);
 		
