01fceb6a6c38b24f06102416f8d699c0fd81a453
rajibroy-wipro <54103367+rajibroy-wipro@users.noreply.github.com>
2021-07-17 19:41:33 +0530

    Update CustomerLoginController.java

--- sm-shop/src/main/java/com/salesmanager/shop/store/controller/customer/CustomerLoginController.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/controller/customer/CustomerLoginController.java
@@ -184,6 +184,7 @@ public class CustomerLoginController extends AbstractController {
 	public @ResponseBody String jsonLogon(@ModelAttribute SecuredCustomer securedCustomer, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("Changes in Login Controller");
 		System.out.println("Changes in Login Controller");
+		System.out.println("Changes in Login Controller");
         AjaxResponse jsonObject = this.logon(securedCustomer.getUserName(), securedCustomer.getPassword(), securedCustomer.getStoreCode(), request, response);
         return jsonObject.toJSONString();
         
